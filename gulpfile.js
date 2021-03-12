e8093ba2542ac648ce5f01d015151dcca763b2f6
Rimantė <rimante.razinskaite@gmail.com>
2021-03-12 13:38:14 +0200

    Add postCSS Autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass");
+const autoprefixer = require("autoprefixer");
+const sourcemaps = require("gulp-sourcemaps");
+const postcss = require("gulp-postcss");
 const rename = require("gulp-rename");
 const browserSync = require("browser-sync").create();
 
@@ -7,9 +10,23 @@ const browserSync = require("browser-sync").create();
 function style() {
   return gulp
     .src("./assets/scss/main.scss")
-    .pipe(sass())
+    .pipe(sass().on("error", sass.logError))
+    .pipe(sourcemaps.init())
+    .pipe(postcss([autoprefixer()]))
+    .pipe(sourcemaps.write("."))
     .pipe(rename("style.css"))
-    .pipe(gulp.dest("./"));
+    .pipe(gulp.dest("./"))
+    .pipe(browserSync.stream());
+}
+
+function watch() {
+  browserSync.init({
+    proxy: "localhost/ugdymoinovacijos",
+  });
+  gulp.watch("./assets/scss/**/*.scss", style);
+  gulp.watch("./**/*.php").on("change", browserSync.reload);
+  gulp.watch("./assets/js/**/*.js").on("change", browserSync.reload);
 }
 
 exports.style = style;
+exports.watch = watch;
