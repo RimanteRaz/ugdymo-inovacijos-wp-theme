6293b124260b438fce45f738ac67b8d483870c29
Rimantė <rimante.razinskaite@gmail.com>
2021-03-22 13:29:46 +0200

    Add footer widget areas

--- functions.php
+++ functions.php
@@ -39,18 +39,33 @@ add_action('wp_enqueue_scripts', 'ugdino_register_scripts');
 
 
 // Add widget areas:
-// function footer_widget_init() {
-//     register_sidebar(array(
-//         'name' => 'Footer Widget Area',
-//         'id' => "footer_1",
-//         'before_widget' => '<div class = "footerWidget">',
-//         'after_widget' => '</div>',
-//         'before_title' => '<h6>',
-//         'after_title' => '</h6>',
-//     ));
-// }
-// 
-// add_action( 'widgets_init', 'footer_widget_init');
+function footer_left_widget_init() {
+    register_sidebar(array(
+        'name' => 'Footer (Left Side)',
+        'id' => "footer_1",
+        'before_widget' => '<div class = "footerWidgetLeft">',
+        'after_widget' => '</div>',
+        'before_title' => '<h5>',
+        'after_title' => '</h5>',
+    ));
+}
+
+add_action( 'widgets_init', 'footer_left_widget_init');
+
+function footer_right_widget_init() {
+    register_sidebar(array(
+        'name' => 'Footer (Right Side)',
+        'id' => "footer_2",
+        'before_widget' => '<div class = "footerWidgetRight">',
+        'after_widget' => '</div>',
+        'before_title' => '<h5>',
+        'after_title' => '</h5>',
+    ));
+}
+
+add_action( 'widgets_init', 'footer_right_widget_init');
+
+// Add Customizer Settings:
 
 function ugdino_nav_cta($wp_customize) {
     $wp_customize->add_section('ugdino-nav-cta-section',array(
@@ -70,7 +85,8 @@ function ugdino_nav_cta($wp_customize) {
     )));
 
     $wp_customize->add_setting('ugdino-nav-cta-text', array(
-        'default' => 'Atsiųskite savo inovaciją'
+        'default' => 'Atsiųskite savo inovaciją',
+        'sanitize_callback' => 'esc_attr',
     ));
 
     $wp_customize->add_control(new WP_Customize_Control($wp_customize, 'ugdino-nav-cta-text-control', array(
@@ -79,7 +95,9 @@ function ugdino_nav_cta($wp_customize) {
         'settings' => 'ugdino-nav-cta-text'
     )));
 
-    $wp_customize->add_setting('ugdino-nav-cta-link');
+    $wp_customize->add_setting('ugdino-nav-cta-link', array(
+        'sanitize_callback' => 'esc_url',
+    ));
 
     $wp_customize->add_control(new WP_Customize_Control($wp_customize, 'ugdino-nav-cta-link-control', array(
         'label' => __('Call To Action button link'),
@@ -90,6 +108,5 @@ function ugdino_nav_cta($wp_customize) {
 }
 
 add_action('customize_register', 'ugdino_nav_cta');
-
 
 ?>
